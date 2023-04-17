2f435a3139e9d27b546c976bdf247c350586ac5a
VertikaJain <vertikajain96@gmail.com>
2023-04-17 16:06:05 +0530

    Added JWT implementation

--- server.js
+++ server.js
@@ -1,15 +1,63 @@
+import dotenv from "dotenv";
 import express from "express";
 import bcrypt from "bcrypt";
+import jsonwebtoken from "jsonwebtoken";
 
+dotenv.config();
 const app = express();
 
 app.use(express.json());
 
 const users = [];
+const weapons = [
+  {
+    username: "krishna",
+    weapon: "sudarshanChakra",
+  },
+  {
+    username: "balram",
+    weapon: "halAndGada",
+  },
+];
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token === null) return res.sendStatus(401);
+  jsonwebtoken.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+};
+
+// Create new Access token if Refresh token is valid
+app.post("/token", (req, res) => {
+  const refreshToken = req.body.refreshToken;
+  if (!refreshToken) res.sendStatus(401);
+  jsonwebtoken.verify(
+    req.body.refreshToken,
+    process.env.REFRESH_TOKEN_SECRET,
+    (err, user) => {
+      if (err) return res.sendStatus(403);
+      const accessToken = jsonwebtoken.sign(
+        { name: user.name },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "20s" }
+      );
+      res.json({ accessToken: accessToken });
+    }
+  );
+});
 
 // GET all users
 app.get("/users", (req, res) => {
   res.json(users);
+});
+
+// GET logged-in user/weapons
+app.get("/weapons", authenticateToken, (req, res) => {
+  res.json(weapons.find((weapon) => weapon.username === req.user.name));
 });
 
 // Add a new user
@@ -28,12 +76,24 @@ app.post("/users/login", async (req, res) => {
   const user = users.find((user) => user.username === req.body.username);
   if (user === null) res.status(400).send("User not Found");
   try {
+    // bcrypt: Authentication
     const isUserAunthenticated = await bcrypt.compare(
       req.body.password,
       user.password
     );
-    if (isUserAunthenticated) res.send("Able to Login");
-    else res.send("Unable to Login");
+    if (isUserAunthenticated) {
+      // JWT: Authorization
+      const accessToken = jsonwebtoken.sign(
+        { name: req.body.username },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "20s" }
+      );
+      const refreshToken = jsonwebtoken.sign(
+        { name: req.body.username },
+        process.env.REFRESH_TOKEN_SECRET
+      );
+      res.json({ accessToken: accessToken, refreshToken: refreshToken });
+    } else res.send("Unable to Login");
   } catch (error) {
     res.status(500).send();
   }
